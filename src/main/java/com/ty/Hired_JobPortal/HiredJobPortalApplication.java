f1c69c9781282967d396c4737793be5d4fe0520f
ram-Ranjan <ranjanirl3107@gmail.com>
2023-05-02 12:32:52 +0530

    Initial Commit ,building the project structure

---
+++ src/main/java/com/ty/Hired_JobPortal/HiredJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ty.Hired_JobPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiredJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiredJobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/Hired_JobPortal/HiredJobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.Hired_JobPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiredJobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
