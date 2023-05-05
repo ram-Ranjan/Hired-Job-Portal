2e8704a0769725f76826347e355f729fbcb64a0d
Ranjan Kumar <127039261+ram-Ranjan@users.noreply.github.com>
2023-05-05 16:38:22 +0530

    Revert "Ranjan"

--- src/main/java/com/ty/Hired_JobPortal/Repo/ResumeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ty.Hired_JobPortal.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.Hired_JobPortal.Entity.Resume;
-
-public interface ResumeRepo extends JpaRepository<Resume, Integer> {
-
-}
--- src/main/java/com/ty/Hired_JobPortal/Repo/SkillRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ty.Hired_JobPortal.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ty.Hired_JobPortal.Entity.Skill;
-
-public interface SkillRepo extends JpaRepository<Skill, Integer>{
-
-}
